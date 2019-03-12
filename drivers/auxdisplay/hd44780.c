1e8138ccdabe42ebc82d0aae769acd8aca13ccfc
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-03-12 16:44:32 +0200

    auxdisplay: hd44780: Convert to use charlcd_free()
    Convert to use charlcd_free() instead of kfree() for sake of type check.
    
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- drivers/auxdisplay/hd44780.c
+++ drivers/auxdisplay/hd44780.c
@@ -289,7 +289,7 @@ static int hd44780_probe(struct platform_device *pdev)
 	return 0;
 
 fail:
-	kfree(lcd);
+	charlcd_free(lcd);
 	return ret;
 }
 
@@ -299,7 +299,7 @@ static int hd44780_remove(struct platform_device *pdev)
 
 	charlcd_unregister(lcd);
 
-	kfree(lcd);
+	charlcd_free(lcd);
 	return 0;
 }
 
