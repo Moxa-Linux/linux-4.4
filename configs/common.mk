daa3c646d5136f1e9f96d4cd8f80a4552b86445c
Remus Wu <remusty.wu@moxa.com>
2021-11-14 16:52:11 +0800

    configs: add common.mk

---
+++ configs/common.mk
@@ -0,0 +1,82 @@
+# SPDX-License-Identifier: Apache-2.0
+
+ARCH ?= arm
+MAKEFLAGS += ARCH=$(ARCH)
+
+ifneq (,$(DEB_HOST_GNU_TYPE))
+CROSS_COMPILE := $(DEB_HOST_GNU_TYPE)-
+else
+CROSS_COMPILE ?= arm-linux-gnueabihf-
+endif
+MAKEFLAGS += CROSS_COMPILE=$(CROSS_COMPILE)
+
+CC ?= $(CROSS_COMPILE)gcc
+MAKEFLAGS += CC=$(CC)
+
+MAKEFLAGS += LOCALVERSION=
+
+KERNEL_DEFCONFIG := $(SOC)_defconfig
+KERNEL_NAME := 4.4.0-cip-rt-moxa-$(SOC)
+
+OUTPUT_DIR := output
+MODULES_OUTPUT_DIR := $(OUTPUT_DIR)/modules
+HEADERS_OUTPUT_DIR := $(OUTPUT_DIR)/headers/linux-headers-$(KERNEL_NAME)
+ITBS_OUTPUT_DIR    := $(OUTPUT_DIR)/itbs
+
+ifneq (,$(DEB_HOST_GNU_TYPE))
+SCRIPTS_OUTPUT_DIR :=  $(OUTPUT_DIR)/kbuild/$(DEB_HOST_GNU_TYPE)/scripts
+else
+SCRIPTS_OUTPUT_DIR :=  $(OUTPUT_DIR)/kbuild/$(CROSS_COMPILE:-=)/scripts
+endif
+
+SCRIPTS_FILES_MAKEFILE := \
+	Makefile.asm-generic \
+	Makefile.build \
+	Makefile.clean \
+	Makefile.dtbinst \
+	Makefile.extrawarn \
+	Makefile.headersinst \
+	Makefile.help \
+	Makefile.host \
+	Makefile.kasan \
+	Makefile.lib \
+	Makefile.modbuiltin \
+	Makefile.modinst \
+	Makefile.modpost \
+	Makefile.modsign \
+	Makefile.ubsan
+
+SCRIPTS_FILES_PL := \
+	checkincludes.pl \
+	checkstack.pl \
+	checkversion.pl \
+	namespace.pl \
+	patch-kernel \
+	recordmcount.pl
+
+SCRIPTS_FILES_SH := \
+	depmod.sh \
+	gcc-goto.sh \
+	gcc-version.sh \
+	gcc-x86_32-has-stack-protector.sh \
+	gcc-x86_64-has-stack-protector.sh \
+	headers_install.sh \
+	ld-version.sh \
+	mkuboot.sh \
+        gen_initramfs_list.sh
+
+SCRIPTS_FILES := \
+	$(SCRIPTS_FILES_MAKEFILE) \
+	$(SCRIPTS_FILES_PL) \
+	$(SCRIPTS_FILES_SH) \
+	Kbuild.include \
+	Lindent \
+	kernel-doc \
+	makelst \
+	mksysmap \
+	mkversion \
+	module-common.lds \
+	setlocalversion \
+	ver_linux
+
+SCRIPTS_FILES_DST := $(addprefix $(SCRIPTS_OUTPUT_DIR)/,$(SCRIPTS_FILES))
